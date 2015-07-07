f02fdd9f1a587b49df999ae09838b4a9f472bc54
wangjun <wangjun@edaijia-inc.cn>
2015-07-07 11:11:58 +0800

    codeUpdate

---
+++ java-top-cpu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export JAVA_HOME='/usr/java/jdk1.7.0_60'
+
+if [ $# -eq 0 ];then
+  echo "please enter java pid"
+  exit -1
+fi
+
+pid=$1
+jstack_cmd=""
+
+if [[ $JAVA_HOME != "" ]]; then
+  jstack_cmd="$JAVA_HOME/bin/jstack"
+else
+  r=`which jstack 2>/dev/null`
+  if [[ $r != "" ]]; then
+    jstack_cmd=$r
+  else
+    echo "can not find jstack"
+    exit -2
+  fi
+fi
+
+#line=`top -H  -o %CPU -b -n 1  -p $pid | sed '1,/^$/d' | grep -v $pid | awk 'NR==2'`
+
+line=`top -H -b -n 1  -p $pid | sed '1,/^$/d' | sed '1d;/^$/d' | grep -v $pid | sort -nrk9 | head -1`
+echo "$line" | awk '{print "tid: "$1," cpu: %"$9}'
+tid_0x=`printf "%0x" $(echo "$line" | awk '{print $1}')`
+$jstack_cmd $pid | grep $tid_0x -A20 | sed -n '1,/^$/p'
