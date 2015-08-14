c1f0ef7176411f0a91ad78e33810fc7bb8b20583
wangjun <wangjun@edaijia-inc.cn>
2015-08-14 14:59:10 +0800

    update

---
+++ mycmd.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#-------------------------------
+# create by wangjun on 2015-07-31
+# wangjuntytl@163.com
+# desc:
+#       快速执行设定的命令，可以直接输入编号进入
+#       例如：
+#               0       ssh node01
+#               1       ssh node02
+#               输入你要进入的终端编号:
+#
+#--------------------------------------------
+usage(){
+        echo '快速执行设定的命令，可以直接输入编号进入
+          例如：
+                       0       ssh node01
+                       1       ssh node02
+                       输入你要进入的终端编号:'
+        echo
+}
+cd `dirname $0`
+[ ! -f "./cmd.list" ] && echo "cmd.list 文件不存在,请先在同级目录下创建cmd.list文件" && exit 1
+CMD_LIST=`cat ./cmd.list`
+echo "${CMD_LIST}" | awk  'BEGIN{print "编号\t命令"}{print NR"\t"$0}END{}'
+echo
+echo "输入你要进入的命令编号:"
+read name
+cmd=`echo  "${CMD_LIST}" | awk  'NR=="'${name}'" {print$0}'`
+echo $cmd
+$cmd
+echo '退出      '$cmd
+echo
+exit 0
---
+++ myscreen.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#-------------------------------
+# create by wangjun on 2015-07-31
+# wangjuntytl@163.com
+# desc:
+#       检测当前所有的screen 进程，并可以直接输入编号进入你需要的resume的screen
+#       例如：
+#               0       20489.pts-115.web04
+#               1       14989.pts-25.web04
+#               输入你要进入的终端编号:
+#
+#--------------------------------------------
+usage(){
+        echo '检测当前所有的screen 进程，并可以直接输入编号进入你需要的resume的screen
+          例如：
+                       0       20489.pts-115.web04
+                       1       14989.pts-25.web04
+                       输入你要进入的终端编号:'
+        echo
+}
+result=`screen -ls | grep 'web04' | awk '{print $1}'`
+status=`screen -ls | grep 'web04' | awk '{print $2}'`
+tems=($result)
+status_seq=($status)
+[ ${#tems[@]}"X" == "0X" ] && echo "没有可用的screen 进程" && exit 1
+echo "编号    名称                     状态"
+for (( i=0; i<${#tems[@]}; i++ ))
+        do
+                echo "$i        ${tems[$i]}     ${status_seq[$i]}"
+        done
+echo "输入你要进入的终端编号:"
+read name
+screen -r ${tems[$name]}
+echo '退出      '${tems[$name]}
+echo
+exit 0
